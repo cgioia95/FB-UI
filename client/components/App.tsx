9b89da5f78d7676d1946a51555fc7376562816c1
Christian <christian.gioia@rea-group.com>
2023-07-01 17:31:48 +1000

    add a dev server with hot reloading

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,4 @@
 // client/components/app.tsx
 import React from 'react'
  
-export const App: React.FC = () => <p>Hello from Client</p>
+export const App: React.FC = () => <p>Hello from Client V3</p>
