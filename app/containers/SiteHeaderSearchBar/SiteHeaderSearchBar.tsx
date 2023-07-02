42594b4f2ec667c70358e5d72691b671274e2660
Christian <christian.gioia@rea-group.com>
2023-07-02 19:02:44 +1000

    add icons, dp and style search bar further

--- app/containers/SiteHeaderSearchBar/SiteHeaderSearchBar.tsx
+++ app/containers/SiteHeaderSearchBar/SiteHeaderSearchBar.tsx
@@ -1,21 +1,32 @@
-import { Clear as ClearIcon, Search as SearchIcon } from '@mui/icons-material';
-import { IconButton, InputAdornment, TextField } from '@mui/material';
+import { Search as SearchIcon } from '@mui/icons-material';
+import { IconButton, InputAdornment, TextField, styled } from '@mui/material';
 import React from 'react';
+
+const StyledSearchBar = styled(TextField)`
+  & .MuiInputBase-input {
+    padding-top: 7px;
+    padding-bottom: 9px;
+    height: 30px;
+  }
+
+  & .MuiInputBase-root {
+    border-radius: 20px;
+    padding-left: 4px;
+    margin-bottom: 6px;
+  }
+
+  & .MuiIconButton-root {
+    padding-right: 0px;
+  }
+`;
 
 const SiteHeaderSearchBar = () => {
   return (
-    <TextField
-      placeholder="Search..."
+    <StyledSearchBar
+      placeholder="Search Facebook"
       InputProps={{
         startAdornment: (
           <InputAdornment position="start">
-            <IconButton>
-              <ClearIcon />
-            </IconButton>
-          </InputAdornment>
-        ),
-        endAdornment: (
-          <InputAdornment position="end">
             <IconButton>
               <SearchIcon />
             </IconButton>
